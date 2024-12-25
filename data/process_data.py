1e069f8d9379525f1a43f05d20577cc00623544d
Hossam Eldin Hassan Salama <hossam9909@yahoo.com>
2024-12-25 14:22:04 +0200

    feat: Created main ETL pipeline in process_data.py
    This commit implements the main ETL pipeline within the process_data.py script.
    The pipeline includes steps for:
      * Loading data from source files
      * Cleaning and transforming the data
      * Saving the cleaned data to a target destination

--- data/process_data.py
+++ data/process_data.py
@@ -56,3 +56,31 @@ def save_cleaned_data(df, database_filepath):
     """
     engine = create_engine(f'sqlite:///{database_filepath}')
     df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')
+
+
+def main():
+    """
+    Main function to execute the ETL pipeline.
+    """
+    if len(sys.argv) == 4:
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(
+            messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_cleaned_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+    else:
+        print('Please provide the filepaths of the messages and categories datasets as the first and second argument '
+              'and the filepath of the database to save the cleaned data to as the third argument. \n\nExample: python '
+              'process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
